1451cd9091f23404d037511962418e2fa5ae3981
Paulo Emilio <superpaco23@gmail.com>
2019-07-25 11:03:48 -0500

    prueba de pagos

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+const { Router } = require('express');
+const router = Router();
+const stripe = require('stripe')('sk_test_ZCrGJ6QiTUoC6csz3fbLZWA300pJeKCf1H');
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post('/checkout', async(req, res)=> {
+    console.log(req.body);
+    const customer = await stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    });
+    const charge = await stripe.charges.create({
+        amount : '3000',
+        currency : 'usd',
+        customer: customer.id,
+        description: 'Test'
+        });
+    console.log(charge.id);
+    //Final show a success view
+    res.render('download');
+});
+
+module.exports = router;
